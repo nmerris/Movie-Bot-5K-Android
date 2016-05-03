89b05202d63a9c7f1c8e9bf886069db701d0f47c
Nathan Merris <nate.merris@gmail.com>
2016-05-03 16:32:12 -0400

    splash screen working, app just stays there forever

--- app/src/main/java/com/nate/moviebot5k/StartupActivity.java
+++ app/src/main/java/com/nate/moviebot5k/StartupActivity.java
@@ -3,12 +3,18 @@ package com.nate.moviebot5k;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartupActivity extends AppCompatActivity {
+    private static final String LOGTAG = SingleFragmentActivity.N8LOG + "StartupActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(SingleFragmentActivity.N8LOG, "just in onCreate");
+
+
 
         // TODO: perform startup tasks here
 
