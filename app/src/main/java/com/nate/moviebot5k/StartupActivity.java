a410c175c9d328d440e41f9fcb7b9248e4eca1ec
Nathan Merris <nate.merris@gmail.com>
2016-05-03 17:20:30 -0400

    added shared_prefs resource file, started initializeSharedPrefs in StartupActivity

--- app/src/main/java/com/nate/moviebot5k/StartupActivity.java
+++ app/src/main/java/com/nate/moviebot5k/StartupActivity.java
@@ -1,10 +1,18 @@
 package com.nate.moviebot5k;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+/**
+ * Initializes this app the first time it is installed on device, and detects if internet is
+ * available.  If internet is available, launches Intent to HomeActivity, if not it will check to
+ * see if user has at least one favorite, and then asks user via an AlertDialog if they would like
+ * to view their favorites.  If so, launches and Intent to FavoritesActivity.
+ */
 public class StartupActivity extends AppCompatActivity {
     private static final String LOGTAG = SingleFragmentActivity.N8LOG + "StartupActivity";
 
@@ -12,11 +20,12 @@ public class StartupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(SingleFragmentActivity.N8LOG, "just in onCreate");
+        Log.i(LOGTAG, "just in onCreate");
 
 
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        // TODO: perform startup tasks here
+
 
 
 
@@ -26,4 +35,13 @@ public class StartupActivity extends AppCompatActivity {
 //        finish();
 
     }
+
+
+    // initialize all sharedPrefs, need this to happen the first time app is installed
+    // or if user clears the app data, they will either ALL exist, or NONE will exist
+    private void initializeSharedPrefs() {
+
+    }
+
+
 }
